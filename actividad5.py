7442bf10b291d7799c20ece35be9b302010ef913
fabri <fabritzioalejandrol@gmail.com>
2025-07-22 09:38:09 -0600

    Menu y sus opciones

---
+++ actividad5.py
@@ -0,0 +1,10 @@
+while True:
+    print("Menu de opciones")
+    print("1. Ingresar lista de ventas")
+    print("2. Mostrar todas las ventas ingresadas")
+    print("3. Calcular venta más alta y baja")
+    print("4. Calcular promedio de ventas")
+    print("5. Contar cuantos dias superaron los Q1000")
+    print("6. Buscar una venta")
+    print("7. Clasificar ventas")
+    print("8. Salir")
