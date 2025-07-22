fb6794b1bb81b1932a8df4dcc2a5bc2f620d0671
fabri <fabritzioalejandrol@gmail.com>
2025-07-22 15:37:39 -0600

    Opcion 1 arreglada y opcion 2

--- actividad5.py
+++ actividad5.py
@@ -6,14 +6,20 @@ while True:
     print("3. Calcular venta más alta y baja")
     print("4. Calcular promedio de ventas")
     print("5. Contar cuantos dias superaron los Q1000")
-    print("6. Buscar una venta")
-    print("7. Clasificar ventas")
-    print("8. Salir")
+    print("6. Clasificar ventas")
+    print("7. Salir")
     opcion= input("Ingrese una de las opciones: ")
     match opcion:
         case "1":
-            dias = int(input("Ingrese cuantos dias por venta quiere ingresar: "))
-            for i in range(dias+1):
-                venta = int(input("Ingrese las ventas de cada dia: "))
-                for j in range(venta+1):
-                    ventas.append((venta))
+            dias = int(input("Ingrese los dias de ventas: "))
+            for i in range(dias):
+                while True:
+                    venta = int(input("Ingrese las ventas de cada dia: "))
+                    ventas.append(venta)
+                    break
+        case "2":
+            print("Ventas ingresadas")
+            ind=0
+            for i in ventas:
+                ind +=1
+                print(f"{ind}. Q{i}")
